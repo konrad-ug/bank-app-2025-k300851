88fe903942ebb9f81905432552d2072759732ec9
=Kamil Żak <=kamilzak19@outlook.com>
2025-11-23 21:39:51 +0100

    lab02

--- src/account.py
+++ src/account.py
@@ -1,4 +1,18 @@
+import re
+from src.national_id_utils import get_year_from_national_id
+
+
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, national_id, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.balance = 0
+
+        national_id_year = get_year_from_national_id(national_id)
+        if national_id_year is None:
+            national_id = "Invalid"
+        self.national_id = national_id
+
+        
+        if promo_code is not None and re.match(r"^PROM_...$", promo_code) and national_id_year and national_id_year > 1960:
+            self.balance += 50
